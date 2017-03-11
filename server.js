7798b08124aa1d18c31d4a127dac167185e415f6
malfuncti0n <savvaniss@yahoo.gr>
2017-03-11 15:54:59 +0200

    allow remote

--- server.js
+++ server.js
@@ -4,10 +4,17 @@ var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+    next();
+})
+
 app.post('/api/message', function(req,res){
     console.log(req.body);
     res.status(200);
 })
+
 var server = app.listen(5000, function(){
     console.log('listening on port ', server.address().port)
 })
