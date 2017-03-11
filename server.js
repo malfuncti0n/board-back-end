2ccdb054dd53d7bfb117782b2bd040bc588f51ab
malfuncti0n <savvaniss@yahoo.gr>
2017-03-11 15:37:24 +0200

    change

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.post('/api/message', function(req,res){
     console.log(req.body);
     res.status(200);
 })
-var server =app.listen(5000, function(){
+var server = app.listen(5000, function(){
     console.log('listening on port ', server.address().port)
 })
