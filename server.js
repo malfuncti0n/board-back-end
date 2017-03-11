fb1efcd8d7ed01fc6fae673b54e94b024ab981fb
malfuncti0n <savvaniss@yahoo.gr>
2017-03-11 15:42:58 +0200

    add json parse

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express');
-var app=express();
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.post('/api/message', function(req,res){
     console.log(req.body);
